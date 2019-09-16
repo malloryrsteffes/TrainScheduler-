b634255136d98da5337080dfe35fcf361783742d
Mal <malrsteffes@gmail.com>
2019-09-16 17:17:27 -0400

    javascript working nicely, moment calculations have been pseudocoded but are nowhere near functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,44 +11,66 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database;
+  var database = firebase.database();
 
-  var name = "";
-  var destination = "";
-  var startTime = "";
-  var frequency = "";
-  var minutesAway = "";
+
 
   $("#submitButton").on("click", function(){
 
     //prevents the page from refreshing
     event.preventDefault();
 
-    name = $("#name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    startTime = moment($("start-time-input").val().trim(), "MM/DD/YYYY").format("X"); //Needs to be in the corrected format
-    // Need to define frequency and minutesAway variables
-    
-    console.log(name, destination, startTime, frequency, minutesAway);
+    var trainName = $("#name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainStartTime = moment($("#start-time-input").val().trim(), "HH:mm").format("X"); 
+    var trainFrequency = "";
+    var trainMinutesAway = "";
 
-    database.ref().push({
-        name: name,
-        destination: destination,
-        startTime: startTime,
-        frequency: frequency,
-        minutesAway: minutesAway
-    })
+    // Saves our variables in a local temporary object 
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination,
+      starts: trainStartTime,
+      frequency: trainFrequency,
+      next: trainMinutesAway
+    };
+
+
+    database.ref().push({newTrain})
+
+    console.log(newTrain.name);
+
+    alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  $("#name-input").val("");
+  $("#destination-input").val("");
+  $("#start-time-input").val("");
+  $("#frequency-input").val("");
 
   })
 
-  database.ref().on("child_added", function(snap){
-      var newTrain = snap.val();
+  database.ref().on("child_added", function(childSnapshot){
 
-      console.log(newTrain.name);
-      console.log(newTrain.destination);
-      console.log(newTrain.startTime);
-      console.log(newTrain.frequency);
-      console.log(newTrain.minutesAway);
+    console.log(childSnapshot.val());
 
+    var trainName = childSnapshot.val().newTrain.name;
+    var trainDestination = childSnapshot.val().newTrain.destination;
+    var trainStartTime = childSnapshot.val().newTrain.starts;
+    var trainFrequency = childSnapshot.val().newTrain.frequency;
 
+    // In order to get the data for the next train, we have to take the trainStartTime, and somehow
+    // add its arrival frequency to it repeatedly until it hits the CURRENT time. The remainder will be how 
+    // many minutes are left until it arrives. If we add that remainder to our current time, we
+    // can see the next arrival time.
+
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(""),
+      $("<td>").text(""),
+    );
+
+    $("tbody").append(newRow);
   })
